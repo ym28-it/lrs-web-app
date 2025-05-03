b1410e5e55a32e44e6a6da91d841c4889a493406
ym28-it <yutamuraya.dev@gmail.com>
2025-05-03 21:02:17 +0900

    rename test for-dev

--- test/test-index.js
+++
@@ -1,8 +0,0 @@
-// test-index.js
-
-function navigateMode(button) {
-    const mode = button.getAttribute('data-mode');
-
-    const targetUrl = `./test-common.html?mode=${encodeURIComponent(mode)}`;
-    window.location.href = targetUrl;
-}

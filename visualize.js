230c0027fe2e5f5441e6f661eeb1817591ce7a3b
ym28-it <120317084+ym28-it@users.noreply.github.com>
2025-07-15 21:12:31 +0900

    start dev visualization

---
+++ visualize.js
@@ -0,0 +1,11 @@
+
+
+export function getVHData() {
+    const outputArea = document.getElementById('outputArea');
+    const inputArea = document.getElementById('inputArea');
+
+    const output = outputArea.value;
+    const input = inputArea.value;
+
+    
+}
